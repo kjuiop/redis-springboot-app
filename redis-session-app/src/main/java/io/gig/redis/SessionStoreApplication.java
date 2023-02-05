f3e64bf1002366f87292be91d06abf250c78c01a
kjuiop <copying_y@naver.com>
2023-02-06 07:43:15 +0900

    [#3] chore: session store module 초기 설정

---
+++ redis-session-app/src/main/java/io/gig/redis/SessionStoreApplication.java
@@ -0,0 +1,17 @@
+package io.gig.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/06
+ */
+@SpringBootApplication
+public class SessionStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionStoreApplication.class, args);
+    }
+
+}
