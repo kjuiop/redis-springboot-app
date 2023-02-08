61be59fe885912610b885d35f73527ad8bae6c8b
kjuiop <copying_y@naver.com>
2023-02-08 09:56:06 +0900

    [#5] chore: cash-layer module create

---
+++ redis-cashlayer-app/src/main/java/io/gig/redis/CashLayerApplication.java
@@ -0,0 +1,17 @@
+package io.gig.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/08
+ */
+@SpringBootApplication
+public class CashLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashLayerApplication.class, args);
+    }
+
+}
