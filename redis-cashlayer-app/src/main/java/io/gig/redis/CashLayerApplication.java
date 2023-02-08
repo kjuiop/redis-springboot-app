a0ba6ffd354972ed53940027510ebd95469b0808
kjuiop <copying_y@naver.com>
2023-02-08 11:05:48 +0900

    [#5] chore: main application @EnableCaching add

--- redis-cashlayer-app/src/main/java/io/gig/redis/CashLayerApplication.java
+++ redis-cashlayer-app/src/main/java/io/gig/redis/CashLayerApplication.java
@@ -2,11 +2,13 @@ package io.gig.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author : JAKE
  * @date : 2023/02/08
  */
+@EnableCaching
 @SpringBootApplication
 public class CashLayerApplication {
 
