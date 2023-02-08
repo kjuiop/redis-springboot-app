24865f3a8312a644bfdf7a7e053b881c7d6df5d6
kjuiop <copying_y@naver.com>
2023-02-08 10:43:18 +0900

    [#5] feat: externalService getUserName, getUserAge func

---
+++ redis-cashlayer-app/src/main/java/io/gig/redis/service/ExternalApiService.java
@@ -0,0 +1,60 @@
+package io.gig.redis.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/08
+ */
+@Service
+@Slf4j
+public class ExternalApiService {
+
+
+    public String getUserName(String userId) {
+
+        // 외부 서비스나 DB 호출했다 가정하고, 네트워크 지연시간 추가
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            log.error("external-api exception");
+        }
+
+        System.out.println("Getting user age from other server .....");
+
+        if (userId.equals("A")) {
+            return "Admin";
+        }
+
+        if (userId.equals("B")) {
+            return "Bob";
+        }
+
+        return "";
+    }
+
+    public int getUserAge(String userId) {
+
+        // 외부 서비스나 DB 호출
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            log.error("external-api exception");
+        }
+
+        System.out.println("Getting user age from other server .....");
+
+        if (userId.equals("A")) {
+            return 28;
+        }
+
+        if (userId.equals("B")) {
+            return 32;
+        }
+
+
+        return 0;
+    }
+}
--- redis-cashlayer-app/src/main/java/io/gig/redis/service/UserService.java
+++ redis-cashlayer-app/src/main/java/io/gig/redis/service/UserService.java
@@ -1,6 +1,7 @@
 package io.gig.redis.service;
 
 import io.gig.redis.dto.UserProfile;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,13 +9,18 @@ import org.springframework.stereotype.Service;
  * @date : 2023/02/08
  */
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final ExternalApiService externalApiService;
 
     public UserProfile getUserProfile(String userId) {
+        String username = externalApiService.getUserName(userId);
+        int userAge = externalApiService.getUserAge(userId);
+
         return UserProfile.builder()
-                .name("username")
-                .age(3)
+                .name(username)
+                .age(userAge)
                 .build();
     }
 }
