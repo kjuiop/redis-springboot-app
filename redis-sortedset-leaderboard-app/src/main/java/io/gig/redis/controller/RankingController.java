820c725e733e97be9c7574660b228828175a49db
kjuiop <copying_y@naver.com>
2023-02-09 23:56:09 +0900

    [#8] feat: setScore api endpoint add

---
+++ redis-sortedset-leaderboard-app/src/main/java/io/gig/redis/controller/RankingController.java
@@ -0,0 +1,29 @@
+package io.gig.redis.controller;
+
+import io.gig.redis.service.RankingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/09
+ */
+@RestController
+@RequiredArgsConstructor
+public class RankingController {
+
+    private final RankingService rankingService;
+
+    @PostMapping("/rank")
+    public Boolean setScore(
+            @RequestParam String userId,
+            @RequestParam int score
+    ) {
+        return rankingService.setUserScore(userId, score);
+    }
+
+
+}
