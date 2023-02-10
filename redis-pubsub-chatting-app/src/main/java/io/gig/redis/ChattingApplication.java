a61d18c9d7cc60d528fb09aaa4822533c8aa6b75
kjuiop <copying_y@naver.com>
2023-02-10 23:57:58 +0900

    [#11] chore: redis-pubsub chatting app 초기 환경 셋팅

---
+++ redis-pubsub-chatting-app/src/main/java/io/gig/redis/ChattingApplication.java
@@ -0,0 +1,17 @@
+package io.gig.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/10
+ */
+@SpringBootApplication
+public class ChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingApplication.class, args);
+    }
+
+}
