447fd237aae5758865b06ebdf5cf7299601f77a6
kjuiop <copying_y@naver.com>
2023-02-11 11:21:52 +0900

    [#11] feat: enterChatRoom 기능, redis pub-sub 연결

--- redis-pubsub-chatting-app/src/main/java/io/gig/redis/ChattingApplication.java
+++ redis-pubsub-chatting-app/src/main/java/io/gig/redis/ChattingApplication.java
@@ -1,5 +1,7 @@
 package io.gig.redis;
 
+import io.gig.redis.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date : 2023/02/10
  */
 @SpringBootApplication
+@RequiredArgsConstructor
 public class ChattingApplication implements CommandLineRunner {
+
+    private final ChatService chatService;
 
     public static void main(String[] args) {
         SpringApplication.run(ChattingApplication.class, args);
@@ -18,5 +23,7 @@ public class ChattingApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Application started...");
+
+        chatService.enterChatRoom("chat-room:1");
     }
 }
--- redis-pubsub-chatting-app/src/main/java/io/gig/redis/service/ChatService.java
+++ redis-pubsub-chatting-app/src/main/java/io/gig/redis/service/ChatService.java
@@ -1,15 +1,42 @@
 package io.gig.redis.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
 
 /**
  * @author : JAKE
  * @date : 2023/02/11
  */
 @Service
+@RequiredArgsConstructor
 public class ChatService implements MessageListener {
+
+    private final RedisMessageListenerContainer container;
+
+    /**
+     * 사용자 이름을 받는 채팅방 기능
+     *
+     */
+    public void enterChatRoom(String chatRoomName) {
+
+        container.addMessageListener(this, new ChannelTopic(chatRoomName));
+
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextLine()) {
+            String line = in.nextLine();
+            if (line.equals("quit")) {
+                System.out.println("Quit...");
+                break;
+            }
+        }
+    }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
