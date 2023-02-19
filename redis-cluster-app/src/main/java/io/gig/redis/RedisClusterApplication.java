9beb031d9dabd2257ffd48d21883297f694e8105
kjuiop <copying_y@naver.com>
2023-02-19 16:35:58 +0900

    [#15] chore: redis-cluster module 초기 환경 구성

---
+++ redis-cluster-app/src/main/java/io/gig/redis/RedisClusterApplication.java
@@ -0,0 +1,17 @@
+package io.gig.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/19
+ */
+@SpringBootApplication
+public class RedisClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisClusterApplication.class, args);
+    }
+
+}
