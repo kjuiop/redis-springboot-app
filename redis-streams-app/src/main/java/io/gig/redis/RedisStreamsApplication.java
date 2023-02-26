51f70e50688b8615e260eec1f16d50d072152254
kjuiop <copying_y@naver.com>
2023-02-26 09:00:34 +0900

    [#18] chore: redis streams module add

---
+++ redis-streams-app/src/main/java/io/gig/redis/RedisStreamsApplication.java
@@ -0,0 +1,17 @@
+package io.gig.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/26
+ */
+@SpringBootApplication
+public class RedisStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStreamsApplication.class, args);
+    }
+
+}
